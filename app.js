d18e88f601ac2687197802a592e139043f12743b
saba-ab <sabaabjandadze@gmail.com>
2023-07-04 20:58:15 +0400

    completed final project

--- app.js
+++ app.js
@@ -42,9 +42,13 @@ function isElementInViewport(element) {
   return rect.top >= 0 && rect.top <= windowHeight - element.offsetHeight;
 }
 // scroll on click
+const fixedHeader = document.querySelector(".header-container");
 function scrollToSection(sectionId) {
   const targetSection = document.getElementById(sectionId);
-  const offsetTop = targetSection.offsetTop - respNavbar.clientHeight;
+  const offsetTop =
+    targetSection.offsetTop -
+    respNavbar.clientHeight -
+    fixedHeader.clientHeight;
   window.scrollTo({
     top: offsetTop,
     behavior: "smooth",
@@ -59,10 +63,10 @@ function toggleBurger() {
   burgerIcon.classList.toggle("active");
   if (respNavbar.style.display === "block") {
     respNavbar.style.display = "none";
-    swiperDiv.style.marginTop = "0px";
+    swiperDiv.style.marginTop = "106px";
   } else {
     respNavbar.style.display = "block";
-    swiperDiv.style.marginTop = `${respNavbar.clientHeight}px`;
+    swiperDiv.style.marginTop = `${respNavbar.clientHeight + 106}px`;
   }
 }
 const responsiveNavigation = respNavbar.querySelectorAll("li");
@@ -76,10 +80,10 @@ function checkResolution() {
 
   if (mediaQuery.matches) {
     respNavbar.style.display = "none";
-    swiperDiv.style.marginTop = `-${respNavbar.clientHeight}px`;
+    swiperDiv.style.marginTop = `-${respNavbar.clientHeight - 106}px`;
   } else {
     respNavbar.style.display = "block";
-    swiperDiv.style.marginTop = `${respNavbar.clientHeight}px`;
+    swiperDiv.style.marginTop = `${respNavbar.clientHeight + 106}px`;
   }
 }
 window.addEventListener("resize", checkResolution);
@@ -133,3 +137,44 @@ submit.addEventListener("submit", function (event) {
   });
   messages.push(newMessage);
 });
+
+// api
+// const fetch = require("node-fetch");
+
+const url = "https://api.brandfetch.io/v2/brands/m2.ge/";
+const url2 = "https://api.brandfetch.io/v2/brands/archi.ge/";
+const url3 = "https://api.brandfetch.io/v2/brands/house.ge//";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: "Bearer TWvnbNVE/eRjWvo05G6PneRYSweS66r2BEJNJ69+hKk=",
+  },
+};
+const partner1 = document.querySelector(".partner1");
+const partner2 = document.querySelector(".partner2");
+const partner3 = document.querySelector(".partner3");
+const partner1Img = document.createElement("img");
+const partner2Img = document.createElement("img");
+const partner3Img = document.createElement("img");
+partner1.appendChild(partner1Img);
+partner2.appendChild(partner2Img);
+partner3.appendChild(partner3Img);
+fetch(url, options)
+  .then((res) => res.json())
+  .then((json) =>
+    partner1Img.setAttribute("src", `${json.logos[0].formats[0].src}`)
+  )
+  .catch((err) => console.error("error:" + err));
+fetch(url2, options)
+  .then((res) => res.json())
+  .then((json) =>
+    partner2Img.setAttribute("src", `${json.logos[0].formats[0].src}`)
+  )
+  .catch((err) => console.error("error:" + err));
+fetch(url3, options)
+  .then((res) => res.json())
+  .then((json) =>
+    partner3Img.setAttribute("src", `${json.logos[0].formats[0].src}`)
+  )
+  .catch((err) => console.error("error:" + err));
