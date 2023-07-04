2761477d7b0de714ac273e2b286fb798ab0d9622
saba-ab <sabaabjandadze@gmail.com>
2023-07-04 05:23:06 +0400

    updated js code

--- app.js
+++ app.js
@@ -44,36 +44,92 @@ function isElementInViewport(element) {
 // scroll on click
 function scrollToSection(sectionId) {
   const targetSection = document.getElementById(sectionId);
-  const offsetTop = targetSection.offsetTop;
+  const offsetTop = targetSection.offsetTop - respNavbar.clientHeight;
   window.scrollTo({
     top: offsetTop,
     behavior: "smooth",
   });
 }
 // burger toggle
+// responsive navbar
 const burgerIcon = document.querySelector(".burger-icon");
-
+const swiperDiv = document.querySelector(".swiper");
+const respNavbar = document.querySelector(".resp-navbar");
 function toggleBurger() {
   burgerIcon.classList.toggle("active");
-}
-// responsive navbar
-const respNavbar = document.querySelector(".resp-navbar");
-
-burgerIcon.addEventListener("click", () => {
   if (respNavbar.style.display === "block") {
     respNavbar.style.display = "none";
+    swiperDiv.style.marginTop = "0px";
   } else {
     respNavbar.style.display = "block";
+    swiperDiv.style.marginTop = `${respNavbar.clientHeight}px`;
   }
+}
+const responsiveNavigation = respNavbar.querySelectorAll("li");
+responsiveNavigation.forEach((item) => {
+  item.addEventListener("click", () => {
+    toggleBurger();
+  });
 });
 function checkResolution() {
   const mediaQuery = window.matchMedia("(min-width: 1241px)");
 
   if (mediaQuery.matches) {
     respNavbar.style.display = "none";
+    swiperDiv.style.marginTop = `-${respNavbar.clientHeight}px`;
   } else {
     respNavbar.style.display = "block";
+    swiperDiv.style.marginTop = `${respNavbar.clientHeight}px`;
   }
 }
-
 window.addEventListener("resize", checkResolution);
+// submit message
+let messages = [];
+const submit = document.getElementById("messageForm");
+submit.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const error = document.getElementById("error");
+  error.style.color = "red";
+  const validated = document.querySelectorAll("input, textarea");
+  const fullName = document.querySelectorAll("input[type='text']");
+  const email = document.querySelector("input[type='email']");
+  const number = document.querySelector("input[type='number']");
+  const message = document.querySelector("textarea");
+  let stopLoop = false;
+  fullName.forEach((name) => {
+    if (stopLoop) return;
+    console.log(name.value);
+    if (/.*\d.*/.test(name.value)) {
+      error.textContent = "სახელი ან გვარი არ უნდა შეიცავდეს ციფრებს";
+      stopLoop = true;
+    } else {
+      error.textContent = "";
+    }
+  });
+  if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value)) {
+    error.textContent = "გთხოვთ შეიყვანოთ მოქმედი მეილი";
+  } else {
+    error.textContent = "";
+  }
+  console.log(number.value.length);
+  if (/^\+995\d{9}$/.test(number.value) || number.value.length !== 12) {
+    error.textContent = "ჩაწერის ფორმატი +995********* გამოიყენეთ 12 ციფრი";
+  } else {
+    error.textContent = "";
+  }
+  validated.forEach((item) => {
+    if (item.value === "") {
+      error.textContent = "გთხოვთ სრულად შეავსოთ ფორმა";
+    } else {
+      error.style.color = "green";
+      error.textContent = "შეტყობინება წარმატებით გაიგზავნა";
+    }
+  });
+  const newMessage = {};
+  validated.forEach((item) => {
+    const key = item.name;
+    const value = item.value;
+    newMessage[key] = value;
+  });
+  messages.push(newMessage);
+});
